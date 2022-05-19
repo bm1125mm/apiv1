4d3604b12c778487ad97f2356ad65246a29b3c31
bm1125mm <bm1125mm@gmail.com>
2022-05-19 12:45:48 -0300

    actualizado productos

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -11,6 +11,36 @@ const getProductsController = async (req, resp = response)=>{
     });
 };
 
+const putProductController = async (req, resp = response) =>{
+    const {nameProducts, descriptionProducts, serialProducts} = req.body
+    const {id} = req.params
+    console.log(id);
+    try{
+const putController = await productsServices.putProducts(id, nameProducts, descriptionProducts, serialProducts);
+return resp.status(200).json({
+    status: 200,
+    message: 'producto actualizado correctamente',
+    putController})        
+    }catch(error){
+        console.log('error', error)
+    }
+}
+
+const deleteProductsController = async (req, resp = response) =>{
+    const {id} = req.params
+    console.log(id);
+    try{
+    const deleteProducts = await productsServices.deleteProducts(id);
+    return resp.status(200).json({
+    deleteProducts,
+    status: 204,
+    message: 'Producto eliminado correctamente'    
+    });
+}catch(error){
+    console.log('error', error)
+}
+}
+
 const postProductsController = async (req, resp = response)=>{
 const {nameProducts, descriptionProducts, serialProducts} = req.body;
 try{
@@ -25,8 +55,26 @@ try{
 }
 }
 
+const patchProductsController = async (req, resp = response) =>{
+    const {nameProducts, descriptionProducts, serialProducts} = req.body;
+    const {id} = req.params
+    console.log(id);
+    try{
+    const patchProducts = await productsServices.patchProducts (id, nameProducts, descriptionProducts, serialProducts);
+    return resp.status (200).json({
+        status: 200,
+        message: 'Producto actualizado',
+        patchProducts
+    })
+    }catch(error){
+        console.log('error', error)
+    }
+}
 
 module.exports = {
     getProductsController,
-    postProductsController
+    putProductController,
+    deleteProductsController,
+    postProductsController,
+    patchProductsController
 }
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,12 +1,21 @@
 const {Router} = require('express');
 const prodRouter = Router();
 const {
-    getProductsController, 
-    postProductsController} = require ('../controllers/products.controller')
+    getProductsController, putProductController,
+    postProductsController,
+    deleteProductsController,
+    patchProductsController} = require ('../controllers/products.controller');
+const { deleteUsersController } = require('../controllers/usuarios.controllers');
 
 prodRouter.get('/lista', getProductsController);
-prodRouter.post('/agregar', postProductsController)
 
+prodRouter.put('/actualizar/:id', putProductController);
+
+prodRouter.delete('/eliminar/:id', deleteProductsController);
+
+prodRouter.post('/agregar', postProductsController);
+
+prodRouter.patch('/actualizado/:id', patchProductsController)
 
 
 module.exports = prodRouter
